adaab0c52e142771b6c6abc1d24b5246d3b0218e
pab1oski211 fernandez <pfernandezb211@gmail.com>
2024-05-09 17:33:54 +0000

    fix:query-with-numbers

--- element.sh
+++ element.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 echo -e "Please provide an element as an argument."
 
-if[[ $1 ]]
+if [[ $1 ]]
 then
 {
-  ## Do a query
-  ELEMENT=$($PSQL "SELECT atomic_number,symbol,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INENR JOIN typer USING(type_id) WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1' ")
+  ## Check if it is number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ## Do a query for string
+  ELEMENT=$($PSQL "SELECT atomic_number,symbol,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number = $1 ")
+  else
+  ## Do a query for string
+  ELEMENT=$($PSQL "SELECT atomic_number,symbol,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1' ")
+  fi
+  echo $ELEMENT
 }
+fi
 
