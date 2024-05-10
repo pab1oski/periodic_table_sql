698de7f472b72a11fb39d5226ced79602bf91383
pab1oski211 fernandez <pfernandezb211@gmail.com>
2024-05-10 11:35:08 +0000

    feat:add-sql-file

--- element.sh
+++ element.sh
@@ -15,8 +15,26 @@ then
   else
   ## Do a query for string
   ELEMENT=$($PSQL "SELECT atomic_number,symbol,name,type,atomic_mass,melting_point_celsius,boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol = '$1' OR name = '$1' ")
+  
   fi
-  echo $ELEMENT
+
+  ## Check if the element exixts
+  if [[ -z $ELEMENT ]]
+  then
+    echo "\nI could not find that element in the database."
+  else
+
+  ## unwrap the output
+
+  echo "$ELEMENT" | while read ATOMIC_NUMBER BAR SYMBOL BAR NAME BAR TYPE BAR ATOMIC_MASS BAR MELTING BAR BOILING
+  do
+    echo "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  done
+
+  fi
+
 }
+else
+  echo -e "Please provide an element as an argument."
 fi
 
